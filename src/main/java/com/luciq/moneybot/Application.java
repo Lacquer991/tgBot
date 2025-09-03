23eb64039b58b8ed1a6c7fb3340ad53d54603a82
Валиуллин Тимур <143969457+Lacquer991@users.noreply.github.com>
2025-09-03 18:16:40 +0300

    Add main Spring Boot application class and properties

--- src/main/java/com/luciq/moneybot/Application.java
+++ src/main/java/com/luciq/moneybot/Application.java
@@ -1,13 +1,18 @@
 package com.luciq.moneybot;
 
+import com.luciq.moneybot.telegram.TelegramProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-public class MoneyBotApplication {
+@EnableConfigurationProperties(TelegramProperties.class)
+@EnableAspectJAutoProxy
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MoneyBotApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
