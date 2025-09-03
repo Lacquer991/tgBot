46290da635ebe8669ccd96c89d38be976cbbfd6a
Валиуллин Тимур <143969457+Lacquer991@users.noreply.github.com>
2025-09-03 18:12:35 +0300

    Add Maven project configuration and wrapper

---
+++ src/test/java/com/luciq/moneybot/MoneyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciq.moneybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
